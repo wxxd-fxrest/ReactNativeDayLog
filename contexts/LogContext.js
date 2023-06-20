85b6099c54f2c40096a348213f23551f481d1b30
wxxd-fxrest <fxrest704@gmail.com>
2023-06-20 15:51:48 +0900

    daylog app 완료

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -1,12 +1,10 @@
 import React from "react";
 import {createContext, useState} from "react";
 import {v4 as uuidv4} from 'uuid';
-// import logsStorage from "../storages/logsStorage";
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-    // const initialLogsRef = useRef(null);
     const [logs, setLogs] = useState(
         Array.from({length: 10}).map((_, index) => ({
             id: uuidv4(),
@@ -38,25 +36,6 @@ export function LogContextProvider({children}) {
         const nextLogs = logs.filter((log) => log.id !== id);
         setLogs(nextLogs);
     };
-
-    // useEffect(() => {
-    //     // useEffect 내에서 async 함수를 만들고 바로 호출
-    //     // IIFE 패턴 
-    //     (async () => {
-    //         const savedLogs = await logsStorage.get();
-    //         if(savedLogs) {
-    //             initialLogsRef.current = savedLogs;
-    //             setLogs(savedLogs);
-    //         }
-    //     }) ();
-    // }, []); 
-
-    // useEffect(() => {
-    //     if(logs === initialLogsRef.current) {
-    //         return ;
-    //     }
-    //     logsStorage.set(logs);
-    // }, [logs]);
 
     return (
         <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
